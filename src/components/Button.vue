d1d2fea9cc94736d31e2e4302bce15149fa88f1a
Ryan McKinstry <ryanmckinstry2@gmail.com>
2022-11-01 19:57:59 -0500

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="$emit('toggle-add-task')" :style="{background: color}" class="btn">{{ title }}</button>
+</template>
+
+<script>
+import { stringLiteral } from "@babel/types";
+
+export default {
+  title: "Button",
+
+  props: {
+    title: String,
+    color: String,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @toggle-add-task="$emit('toggle-add-task')" 
+            :title="showAddTaskValue ? 'Close' : 'AddTask'" 
+            :color="showAddTaskValue ? 'red' : 'green'" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props:{
+    title: String,
+    showAddTaskValue: Boolean,
+  },
+  components: {
+    Button,
+  },
+  emits:['toggle-add-task']
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
